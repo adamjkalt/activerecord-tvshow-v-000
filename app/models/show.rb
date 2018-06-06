fec51bcdf40a3460ce3229580ee6ee45654c3ca8
Adam Kalt <adam.kalt.1@gmail.com>
2018-06-06 22:03:13 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -21,6 +21,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.popular_shows
+    Show.where("rating > 5")
   # returns an array of all of the shows that have a rating greater than
   # 5. hint: use the where Active Record method.
   end
